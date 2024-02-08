e78879cb6e430acdb596ae7f3633deb9d72c7914
zalexki <haumalex@gmail.com>
2024-02-08 22:06:12 +0100

    fix

--- src/Xebot/Converter/DateConverter.cs
+++ src/Xebot/Converter/DateConverter.cs
@@ -6,12 +6,12 @@ public static class DateConverter
 {
     public static string ConvertSecondsToHumanHourReadable(ulong seconds)
     {
-        return TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
+        return TimeSpan.FromSeconds(seconds).ToString(@"HH\:mm\:ss");
     }
 
     public static string ConvertUtcToParisTimeHumanReadable(DateTime utcDate)
     {
         TimeZoneInfo parisTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time");
-        return TimeZoneInfo.ConvertTimeFromUtc(utcDate, parisTimeZone).ToString("dd/MM hh:mm:ss");
+        return TimeZoneInfo.ConvertTimeFromUtc(utcDate, parisTimeZone).ToString("dd/MM HH:mm:ss");
     }
 }
